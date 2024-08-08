bba72a0f63fee88343a65bfc890fea5d7a58a057
vinhadelli <nathanvinhadelli@gmail.com>
2024-08-07 22:32:12 -0300

    Java Project

---
+++ src/main/java/com/ntd/practical_test_ntd_backend/PracticalTestNtdBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ntd.practical_test_ntd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestNtdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTestNtdBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ntd/practical_test_ntd_backend/PracticalTestNtdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntd.practical_test_ntd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTestNtdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
