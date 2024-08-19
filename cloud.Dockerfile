663ecd066900588c1c82b7563185ec851134b948
vinhadelli <nathanvinhadelli@gmail.com>
2024-08-18 22:45:21 -0300

    Docker Working

---
+++ cloud.Dockerfile
@@ -0,0 +1,13 @@
+FROM eclipse-temurin:22.0.2_9-jdk-noble AS build
+WORKDIR /app
+COPY .mvn/ ./.mvn
+COPY mvnw pom.xml ./
+RUN ./mvnw dependency:go-offline
+COPY src ./src
+RUN ./mvnw package -DskipTests -DdbUrl=jdbc:mysql://ec2-18-118-217-66.us-east-2.compute.amazonaws.com:3306/NTD_CALCULATOR
+
+FROM eclipse-temurin:22.0.2_9-jdk-noble
+WORKDIR /app
+COPY --from=build /app/target/practical-test-ntd-backend-0.0.1-SNAPSHOT.jar /app/practical-test-ntd-backend-0.0.1-SNAPSHOT.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar","/app/practical-test-ntd-backend-0.0.1-SNAPSHOT.jar"]
---
+++ src/test/java/com/ntd/practical_test_ntd_backend/services/UserServiceTest.java
@@ -0,0 +1,59 @@
+package com.ntd.practical_test_ntd_backend.services;
+
+import com.ntd.practical_test_ntd_backend.dto.UserDTO;
+import com.ntd.practical_test_ntd_backend.entities.User;
+import com.ntd.practical_test_ntd_backend.persistence.interfaces.IUserRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.BDDMockito.given;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    private IUserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    private User user;
+    private UserDTO userDTO;
+
+    @BeforeEach
+    public void setup(){
+        //employeeRepository = Mockito.mock(EmployeeRepository.class);
+        //employeeService = new EmployeeServiceImpl(employeeRepository);
+        user = new User(1L, "user@test.com", "password", true);
+        userDTO = new UserDTO("user@test.com", "password");
+    }
+
+    // JUnit test for createUser, it also tests if the balance was added!
+    @DisplayName("JUnit test for createUser method")
+    @Test
+    public void createUserTest(){
+        given(userRepository.findByUsername(user.getUsername())).willReturn(Optional.of(user));
+        given(userRepository.save(user)).willReturn(user);
+
+        userService.createUser(userDTO);
+
+        User savedUser = userService.getByUsername(userDTO.getUsername());
+        Double userBalance = userService.getUserBalance(savedUser.getId());
+
+        assertThat(savedUser).isNotNull();
+        assertThat(userBalance).isEqualTo(20.0);
+    }
+}
